a31c9b58376afc708a3e97f8d9a5509e18f90224
Ali <veliev.ali.velievich@gmail.com>
2022-12-01 11:06:00 +0200

    test

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-print('Hello, world!')
+from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton
+from updater import *
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        QMainWindow.__init__(self)
+
+        self.resize(250, 250)
+
+        button = QPushButton('Update', self)
+        button.move(50,50)
+        button.clicked.connect(self.on_click)
+
+        self.show()
+
+    def on_click(self):
+        updateNow()
+
+
+if __name__ == '__main__':
+    import sys
+
+    app = QApplication(sys.argv)
+    win = MainWindow()
+
+    sys.exit(app.exec_())
